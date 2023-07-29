610ff115fb27ba20592f87e9f0d0709a5a103b7b
Yevhenii Shapoval <RobertPath29@gmail.com>
2023-07-29 17:55:51 +0300

    add audin on online

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -15,6 +15,19 @@ class MessageController {
         try {
             const { from_Whom, whom, message, date, nameTableMessage } =
                 req.body;
+            const online = await db.query(
+                `
+                SELECT * FROM person where login = $1
+                `,
+                [whom]
+            );
+            if (!online.rows[0].online) {
+                db.query(
+                    `
+                INSERT INTO table_unread_message_${whom.toLowerCase()} (name_friend) VALUES ($1) RETURNING *`,
+                    [from_Whom]
+                );
+            }
             const response = await db.query(
                 `INSERT INTO table_message_${nameTableMessage} (from_whom, whom, message, date) VALUES ($1, $2, $3, $4) RETURNING *`,
                 [from_Whom, whom, message, date]
